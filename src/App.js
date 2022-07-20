4bd9d191ed041c25b54a32c9aa01ed295924927a
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-07-21 00:34:05 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { Component } from 'react';
 import './App.css';
 import Counter from './counter';
 import NewForm from './form';
 import { Time } from './time';
 
-function App() {
-  return (
-    <div>
-      {/* <Counter /> */}
-      {/* <Time /> */}
-      <NewForm />
-    </div>
-  );
+class App extends Component {
+  formSubmitHahdler = data => {
+    console.log('app', data);
+  };
+  render() {
+    return (
+      <div>
+        {/* <Counter /> */}
+        {/* <Time /> */}
+        <NewForm onSubmit={this.formSubmitHahdler} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/form/index.jsx
+++ src/form/index.jsx
@@ -1,40 +1,103 @@
 import { Component } from 'react';
+import shortid from 'shortid';
 
 class NewForm extends Component {
   state = {
     currentName: '',
-    value: 0,
+    value: '',
+    exp: 'junior',
+    license: false,
   };
+
+  nameInputID = shortid.generate();
+  valueInputID = shortid.generate();
 
   handleChange = e => {
     const { name, value } = e.currentTarget;
-    console.log(name);
-    console.log(value);
+
     this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    e.currentTarget.reset();
+  };
+
+  handleLicenseChange = e => {
+    console.log(e.currentTarget.checked);
+    this.setState({ license: e.currentTarget.checked });
   };
 
   render() {
     return (
       <div className="container">
-        <form>
-          <label>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor={this.nameInputID}>
             Имя{' '}
             <input
               type="text"
               name="currentName"
               value={this.state.name}
               onChange={this.handleChange}
+              id={this.nameInputID}
             />
           </label>
-          <label>
+          <label htmlFor={this.valueInputID}>
             Значение{' '}
             <input
-              type="number"
+              type="text"
               name="value"
               value={this.state.name}
               onChange={this.handleChange}
+              id={this.valueInputID}
             />
           </label>
+          <p>Ваш уровень</p>
+          <label>
+            <input
+              type="radio"
+              name="exp"
+              value="junior"
+              onChange={this.handleChange}
+              checked={this.state.exp === 'junior'}
+            />{' '}
+            Junior
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              name="exp"
+              value="middle"
+              onChange={this.handleChange}
+              checked={this.state.exp === 'middle'}
+            />
+            Middle
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="exp"
+              value="senior"
+              onChange={this.handleChange}
+              checked={this.state.exp === 'senior'}
+            />
+            Senior
+          </label>
+          <br />
+          <label>
+            <input
+              type="checkbox"
+              name="license"
+              checked={this.state.license}
+              onChange={this.handleLicenseChange}
+            />
+            Согласен
+          </label>
+          <button type="submit" disabled={!this.state.license}>
+            Отправить
+          </button>
         </form>
       </div>
     );
